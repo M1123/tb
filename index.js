bae699995406ca48a6291610ff8cc16b50b7fe9c
M1123 <dbv93@yandex.ru>
2020-04-18 20:24:32 +0300

    log

--- index.js
+++ index.js
@@ -5,5 +5,6 @@ const token = '1263883084:AAGc2HmKWuABLlys4S-XUj6olaHo00JOOLQ';
 const bot = new TB(token, {polling:true});
 
 bot.on('message', msg => {
-    bot.sendMessage(msg.chat.id, `Hello, ${msg.from.first_name}`)
-})
+    bot.sendMessage(msg.chat.id, `Hello, ${msg.from.first_name}`);
+    bot.sendMessage(msg.chat.id, `msg: ${msg}`);
+});
