f832f83905f3b9c096f7687247e8a17d3373a3b4
M1123 <dbv93@yandex.ru>
2020-04-18 19:22:19 +0300

    inital

---
+++ index.js
@@ -0,0 +1,9 @@
+const TB = require('node-telegram-bot-api')
+
+const token = '1263883084:AAGc2HmKWuABLlys4S-XUj6olaHo00JOOLQ';
+
+const bot = new TB(token, {polling:true});
+
+bot.on('message', msg => {
+    bot.sendMessage(msg.chat.id, `Hello, ${msg.from.first_name}`)
+})
